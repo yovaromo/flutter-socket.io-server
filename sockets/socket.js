7703163101f2ebaf9d94af9abcf26020af0a8481
Yovany Romo <yovaromo@gmail.com>
2021-07-13 16:45:08 -0500

    Update Sockets

--- sockets/socket.js
+++ sockets/socket.js
@@ -3,14 +3,14 @@ const { io } = require('../index');
 //Mensaje de Sockets
 io.on('connection', client =>{
 
-    console.log('Cliente conectado');
+    console.log('Cliente conectado [node]');
    
     client.on('disconnect', () =>{
-        console.log('Clinete desconectado');
+        console.log('Clinete desconectado [node]');
     });
 
     client.on('mensaje', (payload) => {
         console.log('Mensaje!!!!', payload.name)
-        io.emit('mensaje', { admin:'nuevio mensaje'});
+        io.emit('mensaje', { admin:'nuevio mensaje [node]'});
     });
 });
